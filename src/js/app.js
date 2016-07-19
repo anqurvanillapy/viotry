2a361233f8fc75dbaa6515e5e5fdb1ab97931769
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-07-20 04:54:35 +0800

    updated demo menu bar

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-(function () {})()
+(function () {
+  var menuButton = document.getElementById('menuButton')
+
+  menuButton.addEventListener('click', function () {
+    if (this.classList.contains('active')) {
+      this.classList.remove('active')
+    } else {
+      this.classList.add('active')
+    }
+  })
+})()
