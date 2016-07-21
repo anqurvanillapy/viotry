b87572fb184931162dff8fd285db2048868a6d40
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-07-22 03:20:39 +0800

    updated test modules and sidebar container

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 (function () {
   var menuButton = document.getElementById('menuButton')
+  var sidebarContainer = document.querySelector('.sidebar-container')
 
   menuButton.addEventListener('click', function () {
     if (this.classList.contains('active')) {
       this.classList.remove('active')
+      sidebarContainer.classList.remove('sidebar-container--visible')
     } else {
       this.classList.add('active')
+      sidebarContainer.classList.add('sidebar-container--visible')
     }
   })
 })()
