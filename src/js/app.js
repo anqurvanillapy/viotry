eb5b663195df3d7b19b3d7b4cc199892258cbd5d
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-07-23 02:35:15 +0800

    updated sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,60 @@
 (function () {
-  var menuButton = document.getElementById('menuButton')
-  var sidebarContainer = document.querySelector('.sidebar-container')
+  'use strict'
 
-  menuButton.addEventListener('click', function () {
-    if (this.classList.contains('active')) {
-      this.classList.remove('active')
-      sidebarContainer.classList.remove('sidebar-container--visible')
+  var app = {
+    hasVisibleSidebar: false,
+    header: document.querySelector('.header'),
+    main: document.querySelector('.main'),
+    menuButton: document.getElementById('menuButton'),
+    sidebarOverlay: document.querySelector('.sidebar-overlay'),
+    sidebar: document.querySelector('.sidebar')
+  }
+
+  /**
+   *  ==========================================================================
+   *  Event Listeners for UI Elements
+   *  ==========================================================================
+   */
+
+  app.header.addEventListener('click', function () {
+    app.backToTop()
+  })
+
+  app.menuButton.addEventListener('click', function () {
+    if (app.hasVisibleSidebar) {
+      app.toggleSideBar(false)
     } else {
-      this.classList.add('active')
-      sidebarContainer.classList.add('sidebar-container--visible')
+      app.toggleSideBar(true)
     }
   })
+
+  app.sidebarOverlay.addEventListener('click', function (e) {
+    if (e.target === app.sidebarOverlay) {
+      app.toggleSideBar(false)
+    }
+  })
+
+  /**
+   *  ==========================================================================
+   *  Methods to Update/Refresh UI
+   *  ==========================================================================
+   */
+
+  app.backToTop = function () {
+    app.main.scrollTop = 0
+  }
+
+  app.toggleSideBar = function (visible) {
+    if (visible) {
+      app.menuButton.classList.add('active')
+      app.sidebarOverlay.classList.add('active')
+      app.sidebar.classList.add('active')
+      app.hasVisibleSidebar = true
+    } else {
+      app.menuButton.classList.remove('active')
+      app.sidebarOverlay.classList.remove('active')
+      app.sidebar.classList.remove('active')
+      app.hasVisibleSidebar = false
+    }
+  }
 })()
